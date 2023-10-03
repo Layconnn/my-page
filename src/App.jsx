fb888d60c016426f52bc3b0242069824cc587882
Layconnn <olamilekanjinadu21@gmail.com>
2023-10-03 14:46:08 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import AppHome from "./components/AppHome";
-import { WebHome } from "./components/WebHome";
-import { VideoReady } from "./components/VideoReady";
-import { VideoLink } from "./components/VideoLink";
+import Routing from "./router/router";
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apphome" element={<AppHome />} />
-        <Route path="/webhome" element={<WebHome />} />
-        <Route path="/videoready" element={<VideoReady />} />
-        <Route path="/videolink" element={<VideoLink />} />
-      </Routes>
-    </Router>
+    <><Routing /></>
   );
 }
 
